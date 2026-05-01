2e203617dd67429b413d44c57d3f6cb617c83461
Jaeger Nolte <jaeger.nolte@gmail.com>
2026-05-01 12:37:10 -0500

    initial commit

---
+++ src/test/java/com/warpedcitadel/fileuploadservice/FileuploadserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warpedcitadel.fileuploadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
